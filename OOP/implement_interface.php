0097617cb9a5494920dcf6ee57ccd6cced3930db
md sium <55883376+mdsium@users.noreply.github.com>
2024-08-13 23:03:16 +0600

    Interface OOP in PHP
    Implements interface

--- OOP/implement_interface.php
+++ OOP/implement_interface.php
@@ -1,4 +1,32 @@
 <?php
 require_once("interface.php");
 
+class Point implements IPoint{
+    private $x;
+    private $y;
+
+    public function __toString(){
+        return "(".$this->x.",".$this->y.")";
+    }
+
+    public function get_x(){
+        return $this->x;
+    }
+
+    public function get_y(){
+        return $this->y;
+    }
+
+    public function set_x($x){
+        $this->x=$x;
+    }
+
+    public function set_y($y){
+        $this->y=$y;
+    }
+
+    public function show_point(){
+       echo "(x,y)=(".$this->x.",".$this->y.")";
+    }
+}
 ?>
