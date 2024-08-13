2befffafe93e9d42acdd8daf85cb93bb72f33135
md sium <55883376+mdsium@users.noreply.github.com>
2024-08-13 23:02:15 +0600

    OOP Interface
    Interface create a implement in PHP

---
+++ OOP/interface.php
@@ -0,0 +1,9 @@
+<?php
+    interface IPoint{
+        public function show_point();//abstract method
+        public function get_x();//abstract method
+        public function get_y();//abstract method
+        public function set_x($x);//abstract method
+        public function set_y($y);//abstract method
+    }
+?>
