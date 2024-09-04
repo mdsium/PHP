b900d8f5b4646e543c5716d11990b7891ca0845c
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-04 23:05:13 +0600

    where row mysql
    select form data any specific id or name

--- php_MySQL/create_table.sql
+++ php_MySQL/create_table.sql
@@ -18,3 +18,5 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
 -- select section sql command 
 SELECT * FROM table_name
+-- using where for specific id or name
+SELECT column_name(s) FROM table_name WHERE column_name operator value
