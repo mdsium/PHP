c628f135173ce920b729dc50ffea19328a9623d9
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-04 23:02:40 +0600

    where php
    select specific id or name

--- php_MySQL/where.php
+++ php_MySQL/where.php
@@ -12,20 +12,23 @@ $dbname = "myDB";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='md'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
-    }
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+  }
 } else {
-    echo "0 results";
+  echo "0 results";
 }
 
 $conn->close();
 ?>
+
+</body>
+</html>
