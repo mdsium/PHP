0dbd88c795f9e0d87ba881533a33a1a057f63bcd
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-07 23:43:24 +0600

    AJAX JavaScript part
    hint from search bar

---
+++ PHP_AJAX/ajax_1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function showHint(str) {
+  if (str.length == 0) { 
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    }
+    xmlhttp.open("GET", "gethint.php?q="+str, true);
+    xmlhttp.send();
+  }
+}
+</script>
+</head>
+<body>
